82bde4ff6d4bfb57e098eb5de9e8c300ac994c4d
Scott Moss <willscottmoss@gmail.com>
2021-12-13 14:54:05 -0600

    db

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+import { artistsData } from './songsData'
+
+const prisma = new PrismaClient()
+
+const run = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => ({
+              name: song.name,
+              duration: song.duration,
+            })),
+          },
+        },
+      })
+    })
+  )
+}
+
+run()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
