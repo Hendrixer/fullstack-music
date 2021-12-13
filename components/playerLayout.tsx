ad4a711c4cc956ff2e1200a6313a0502eb387a1f
Scott Moss <willscottmoss@gmail.com>
2021-12-13 11:05:34 -0600

    layout

---
+++ components/playerLayout.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/layout";
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box>
+      Layout
+      {children}
+    </Box>
+  );
+};
+
+export default PlayerLayout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import PlayerLayout from "../components/playerLayout";
 import "reset-css";
 
 const theme = extendTheme({
@@ -32,7 +33,9 @@ const theme = extendTheme({
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />;
+      <PlayerLayout>
+        <Component {...pageProps} />
+      </PlayerLayout>
     </ChakraProvider>
   );
 };
