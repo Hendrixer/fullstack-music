261af95f274711e87c5f85c29e0958703dcf2a81
Scott Moss <willscottmoss@gmail.com>
2021-12-13 11:18:02 -0600

    layout

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -2,9 +2,16 @@ import { Box } from "@chakra-ui/layout";
 
 const PlayerLayout = ({ children }) => {
   return (
-    <Box>
-      Layout
-      {children}
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
+        sidebar
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        {children}
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        player
+      </Box>
     </Box>
   );
 };
