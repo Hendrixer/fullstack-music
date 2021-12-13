e1d58a757fdb5eb3f1b6d9294ce72b7e00fc3f8a
Scott Moss <willscottmoss@gmail.com>
2021-12-13 10:22:36 -0600

    hello

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'airbnb', 'airbnb/hooks', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'consistent-return': 'off',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'prettier/prettier': 'warn',
+  },
+}
