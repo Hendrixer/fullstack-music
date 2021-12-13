98a6b8de25b98e39e35e36342837ec943ddb0920
Scott Moss <willscottmoss@gmail.com>
2021-12-13 15:08:29 -0600

    user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -16,12 +16,23 @@ const run = async () => {
             create: artist.songs.map((song) => ({
               name: song.name,
               duration: song.duration,
+              url: song.url,
             })),
           },
         },
       })
     })
   )
+
+  const salt = bcrypt.genSaltSync()
+  const user = await prisma.user.upsert({
+    where: { email: 'user@test.com' },
+    update: {},
+    create: {
+      email: 'user@test.com',
+      password: bcrypt.hashSync('password', salt),
+    },
+  })
 }
 
 run()
