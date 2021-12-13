14b14dbbbe2700cb24cce7a4444fd7f033c6878a
Scott Moss <willscottmoss@gmail.com>
2021-12-13 10:58:05 -0600

    step-2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,40 @@
-import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "reset-css";
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#F5f5f5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+});
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
