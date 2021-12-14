359de17aa314653fa47c55358eecf8b026c15769
Scott Moss <willscottmoss@gmail.com>
2021-12-14 14:07:58 -0600

    home page with artist

--- components/gradientLayout.tsx
+++ components/gradientLayout.tsx
@@ -33,6 +33,7 @@ const GradientLayout = ({
           <Text fontSize="x-small">{description}</Text>
         </Box>
       </Flex>
+      <Box paddingY="50px">{children}</Box>
     </Box>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,52 @@
+import { Box, Text, Flex } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/react'
 import GradientLayout from '../components/gradientLayout'
+import prisma from '../lib/prisma'
 
-const Home = () => {
+const Home = ({ artists }) => {
   return (
     <GradientLayout
       roundImage
-      color="green"
+      color="gray"
       subtitle="profile"
       title="Scott Moss"
       description="15 public playlists"
       image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
     >
-      <div>home page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   )
+}
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({})
+
+  return {
+    props: { artists },
+  }
 }
 
 export default Home
