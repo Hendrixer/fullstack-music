aa6d6a7c8000e78852d487efdb46c0d5c835bf43
Scott Moss <willscottmoss@gmail.com>
2021-12-14 10:03:44 -0600

    fetchre

---
+++ lib/fetcher.ts
@@ -0,0 +1,10 @@
+export default function fetcher(url: string, data = undefined) {
+  return fetch(`${window.location.origin}/api${url}`, {
+    method: data ? 'POST' : 'GET',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+}
