31a6e17201960b94858f3de051a189bf2a5d8904
Scott Moss <willscottmoss@gmail.com>
2021-12-15 11:23:37 -0600

    player seek

--- components/player.tsx
+++ components/player.tsx
@@ -74,6 +74,31 @@ const Player = () => {
           />
         </ButtonGroup>
       </Center>
+
+      <Box color="gray.600">
+        <Flex justify="center" align="center">
+          <Box width="10%">
+            <Text fontSize="xs">1:21</Text>
+          </Box>
+          <Box width="80%">
+            <RangeSlider
+              aria-label={['min', 'max']}
+              step={0.1}
+              min={0}
+              max={321}
+              id="player-range"
+            >
+              <RangeSliderTrack bg="gray.800">
+                <RangeSliderFilledTrack bg="gray.600" />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+            </RangeSlider>
+          </Box>
+          <Box width="10%" textAlign="right">
+            <Text fontSize="xs">321</Text>
+          </Box>
+        </Flex>
+      </Box>
     </Box>
   )
 }
