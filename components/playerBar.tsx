7e48b0455967e13bcc4e590bdd0e7790c4c6298a
Scott Moss <willscottmoss@gmail.com>
2021-12-15 10:24:02 -0600

    fix auth srever side

---
+++ components/playerBar.tsx
@@ -0,0 +1,7 @@
+import { Box, Flex } from '@chakra-ui/layout'
+
+const PlayerBar = () => {
+  return <Box>hello</Box>
+}
+
+export default PlayerBar
--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -36,11 +36,23 @@ const Playlist = ({ playlist }) => {
 }
 
 export const getServerSideProps = async ({ query, req }) => {
-  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN)
+  let user
+
+  try {
+    user = validateToken(req.cookies.TRAX_ACCESS_TOKEN)
+  } catch (e) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/signin',
+      },
+    }
+  }
+
   const [playlist] = await prisma.playlist.findMany({
     where: {
       id: +query.id,
-      userId: id,
+      userId: user.id,
     },
     include: {
       songs: {
