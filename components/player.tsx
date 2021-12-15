716fb6f82ba78f201c452dbce8d50dbf0e886733
Scott Moss <willscottmoss@gmail.com>
2021-12-15 14:00:21 -0600

    seek

--- components/player.tsx
+++ components/player.tsx
@@ -21,6 +21,7 @@ import {
   MdOutlineRepeat,
 } from 'react-icons/md'
 import { useStoreActions } from 'easy-peasy'
+import { formatTime } from '../lib/formatters'
 
 const Player = ({ songs, activeSong }) => {
   const [playing, setPlaying] = useState(true)
@@ -31,6 +32,22 @@ const Player = ({ songs, activeSong }) => {
   const [shuffle, setShuffle] = useState(false)
   const [duration, setDuration] = useState(0.0)
   const soundRef = useRef(null)
+
+  useEffect(() => {
+    let timerId
+
+    if (playing && !isSeeking) {
+      const f = () => {
+        setSeek(soundRef.current.seek())
+        timerId = requestAnimationFrame(f)
+      }
+
+      timerId = requestAnimationFrame(f)
+      return () => cancelAnimationFrame(timerId)
+    }
+
+    cancelAnimationFrame(timerId)
+  }, [playing, isSeeking])
 
   const setPlayState = (value) => {
     setPlaying(value)
@@ -92,7 +109,7 @@ const Player = ({ songs, activeSong }) => {
           src={activeSong?.url}
           ref={soundRef}
           onLoad={onLoad}
-          onEnd={onEnd} 
+          onEnd={onEnd}
         />
       </Box>
       <Center color="gray.600">
@@ -159,7 +176,7 @@ const Player = ({ songs, activeSong }) => {
       <Box color="gray.600">
         <Flex justify="center" align="center">
           <Box width="10%">
-            <Text fontSize="xs">1:21</Text>
+            <Text fontSize="xs">{formatTime(seek)}</Text>
           </Box>
           <Box width="80%">
             <RangeSlider
@@ -180,7 +197,7 @@ const Player = ({ songs, activeSong }) => {
             </RangeSlider>
           </Box>
           <Box width="10%" textAlign="right">
-            <Text fontSize="xs">321</Text>
+            <Text fontSize="xs">{formatTime(duration)}</Text>
           </Box>
         </Flex>
       </Box>
