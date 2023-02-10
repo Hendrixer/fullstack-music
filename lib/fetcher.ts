992bb82442927c5cae2e2da56386b57d37b7d295
Raj Aryan <65965202+Creatoon@users.noreply.github.com>
2023-02-11 04:14:24 +0530

    Fix: it will always be false unless

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -7,7 +7,7 @@ export default function fetcher(url: string, data = undefined) {
     },
     body: JSON.stringify(data),
   }).then((res) => {
-    if (res.status > 399 && res.status < 200) {
+    if (res.status > 399 || res.status < 200) {
       throw new Error()
     }
     return res.json()
